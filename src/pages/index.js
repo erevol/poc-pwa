6ed6bf02450bb6c0c787d0c93f99d9ff9548b1ab
Eugenia Revol <euge.revol@gmail.com>
2024-01-03 20:02:04 -0300

    Modify content

--- src/pages/index.js
+++ src/pages/index.js
@@ -89,9 +89,10 @@ const IndexPage = () => {
   return (
     <main style={{ fontFamily: 'sans-serif'}}>
       <div style={{ margin: '30px', color: '#2d3436'}}>
-        <h1>Proof of Concept 👋</h1>
+        <h1>POC PWA 👋</h1>
         <h2>What is a Progressive Web App?</h2>
-        <p>“Progressive web app” (PWA) is both a general term for a new philosophy toward building websites and a specific term with an established set of three explicit, testable, baseline requirements.</p>
+        <p>A <strong>progressive web app (PWA)</strong> is an app that's built using web platform technologies, but that provides a user experience like that of a platform-specific app.</p>
+        <p>Like a website, a PWA can run on multiple platforms and devices from a single codebase. Like a platform-specific app, it can be installed on the device, can operate while offline and in the background, and can integrate with the device and with other installed apps.</p>
       </div>
       <InstallModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} />
     </main>
